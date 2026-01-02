0a8e8e4069602269d22f4d09efdab769f0ccfe52
Thijs Kuilman <thijs.kuilman@gmail.com>
2026-01-02 21:11:49 +0100

    Create enums

---
+++ app/Enums/DeliveryChannel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum DeliveryChannel: string
+{
+    case Booklore = 'booklore';
+    case Instapaper = 'instapaper';
+}
---
+++ app/Enums/SourceType.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+enum SourceType: string
+{
+    case RSS = 'rss';
+}
