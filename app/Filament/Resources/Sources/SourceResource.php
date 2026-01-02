2a0532f531abfd482808bc30e9cd7d634b7b3869
Thijs Kuilman <thijs.kuilman@gmail.com>
2026-01-02 21:35:20 +0100

    Updated icon

--- app/Filament/Resources/Sources/SourceResource.php
+++ app/Filament/Resources/Sources/SourceResource.php
@@ -18,7 +18,7 @@ class SourceResource extends Resource
 {
     protected static ?string $model = Source::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::Rss;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::Signal;
 
     protected static ?string $recordTitleAttribute = 'name';
 
