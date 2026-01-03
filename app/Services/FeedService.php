e7811f64d041f5e5088b837a3ccd5fdd0adb03dd
Thijs Kuilman <thijs.kuilman@gmail.com>
2026-01-03 16:19:05 +0100

    Add FeedService which fetches articles from a given RSS feed

---
+++ app/Services/FeedService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Article;
+use App\Models\Source;
+use Illuminate\Support\Collection;
+
+class FeedService
+{
+    public function storeArticlesFromSource(Source $source, int $articleLimit = 5): void
+    {
+        $items =  $this->fetchRssItems($source->url);
+
+        foreach ($items->take(limit: $articleLimit) as $item) {
+            Article::firstOrCreate(
+                ['url' => $item['permalink']],
+                [
+                    'source_id' => $source->id,
+                    'title' => $item['title'],
+                    'excerpt' => $item['description'],
+                    'published_at' => $item['date'],
+                ]
+            );
+        }
+    }
+
+    private function fetchRssItems(string $feedUrl): Collection {
+        $feed = \Feeds::make(feedUrl: $feedUrl, limit: 5);
+
+        return collect($feed->get_items())->map(function ($item) {
+            return [
+                'permalink' => $item->get_permalink(),
+                'title' => $item->get_title(),
+                'description' => $item->get_description(),
+                'date' => $item->get_date(),
+            ];
+        });
+    }
+}
