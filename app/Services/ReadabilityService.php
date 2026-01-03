93163935105c8e2d2977b7bb2e46e5b59c9cb955
Thijs Kuilman <thijs.kuilman@gmail.com>
2026-01-03 16:19:38 +0100

    Add ReadabilityService which gets the HTML of the article and stores it

---
+++ app/Services/ReadabilityService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Services;
+
+use App\Enums\ArticleStatus;
+use App\Models\Article;
+use andreskrey\Readability\Readability;
+use andreskrey\Readability\Configuration;
+use andreskrey\Readability\ParseException;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizer;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizerConfig;
+
+class ReadabilityService
+{
+    public function parseArticleContent(Article $article): void
+    {
+        $readability = new Readability(new Configuration());
+
+        $html = file_get_contents($article->url);
+
+        try {
+            $readability->parse($html);
+
+            $content =  $readability->getContent();
+
+            $article->update([
+                'html_content' => $content,
+                'status' => ArticleStatus::Parsed->value,
+                'image' => $readability->getImage(),
+            ]);
+
+        } catch (ParseException $e) {
+            $article->update(['status' => ArticleStatus::Failed->value]);
+            echo sprintf('Error processing text: %s', $e->getMessage());
+        }
+    }
+
+    private function sanitizeHtml(string $html): string
+    {
+        $config = new HtmlSanitizerConfig();
+
+        // Allow safe standard elements like <p>, <b>, <i>, etc.
+        $config = $config->allowSafeElements();
+
+        $elements = ['h1','h2','h3','h4','h5','h6','ul','ol','li','figure','figcaption','img','a'];
+        foreach ($elements as $el) {
+            $config = $config->allowElement($el, '*'); // '*' = all standard attributes
+        }
+
+        $config = $config
+            ->allowAttribute('src', ['img'])
+            ->allowAttribute('alt', ['img'])
+            ->allowAttribute('href', ['a'])
+            ->allowAttribute('title', ['a']);
+
+        $sanitizer = new HtmlSanitizer($config);
+
+        return $sanitizer->sanitize($html);
+    }
+
+}
