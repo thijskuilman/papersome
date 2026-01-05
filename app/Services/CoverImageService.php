6d1a49dbe413958b5b76e062c7d44d3cbdd9efbe
Thijs Kuilman <thijs.kuilman@gmail.com>
2026-01-05 18:33:08 +0100

    Add CoverImageService which can generate a cover image using Browsershot

---
+++ app/Services/CoverImageService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Publication;
+use Illuminate\Support\Str;
+use Spatie\Browsershot\Browsershot;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
+
+class CoverImageService
+{
+    public function generateCoverImage(Publication $publication): ?string
+    {
+        $fileName = 'cover-' . Str::slug($publication->collection->name . '-' . $publication->id) . '.png';
+        $relativePath = 'epubs/covers/' . $fileName;
+        $fullPath = Storage::disk('public')->path($relativePath);
+
+        if (!is_dir(dirname($fullPath))) {
+            mkdir(dirname($fullPath), 0755, true);
+        }
+
+        $url = URL::route('cover.generate', [
+            'publication' => $publication->id
+        ]);
+
+        try {
+            Browsershot::url($url)
+                ->windowSize(600, 800)
+                ->waitUntilNetworkIdle()
+                ->save($fullPath);
+
+            return $relativePath;
+        } catch (\Exception $e) {
+            // TODO: Error logging
+            return null;
+        }
+    }
+}
