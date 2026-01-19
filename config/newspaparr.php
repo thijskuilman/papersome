3aa9856655379832964b09120b556736c25d454e
Thijs Kuilman <thijs.kuilman@gmail.com>
2026-01-19 17:06:58 +0100

    Added Booklore retention hours to specify how many hours after deletion in Newspaparr the change should be propagated to Booklore.

---
+++ config/newspaparr.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'booklore_retention_hours' => env('BOOKLORE_RETENTION_HOURS'),
+];
---
+++ database/settings/2026_01_19_161700_booklore_delete_after_hours.php
@@ -0,0 +1,11 @@
+<?php
+
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
+
+return new class extends SettingsMigration
+{
+    public function up(): void
+    {
+        $this->migrator->add('application.booklore_retention_hours', 8);
+    }
+};
